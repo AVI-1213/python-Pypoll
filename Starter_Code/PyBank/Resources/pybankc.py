477c782be1c4b7b587cf8c97ebd369f117bff03b
AVI-1213 <avinashsingh1213@icloud.com>
2024-03-20 23:10:42 -0400

    update code file ,deleted print command

--- Starter_Code/PyBank/Resources/pybankc.py
+++ Starter_Code/PyBank/Resources/pybankc.py
@@ -1,3 +1,6 @@
+
+# please read code from visiting each folder Path individually`./python-challenge/Starter_Code/PyBank/Resources`
+
 # importing the module OS and CSV
 import os
 import csv
@@ -18,11 +21,8 @@ with open(filepath) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csv_reader, None)
     
-    # for row in csv_reader:
-    #     print(row[0:])
-
 # # solution 1: 
-#Total number of month & Net total profit & loss
+# Looping for Total number of month & Net total profit & loss
 
     for row in csv_reader :
         monthCount.append(row[0])
@@ -30,12 +30,10 @@ with open(filepath) as csvfile:
 
 # total number of month count
     totalMonth = len(monthCount)
-    # print ("Total Months : ", totalMonth)
-
+    
 # Solution 2 : The net total amount of "Profit/Losses" over the entire period
     totalPnL = sum(PnL)
-    # print("Net Total amount of Profit/loss : ", totalPnL)
-
+    
 
 # Solution 3: The changes in "Profit/Losses" over the entire period, and then the average of those changes
 # setting variable for loop - # averageChange = (totalPnL[1]- totalPnL[0])
@@ -43,7 +41,7 @@ with open(filepath) as csvfile:
 x = 1
 y = 0
 
-for month in range(totalMonth-1):
+for month in range(totalMonth-1): # -1 upto the last range index
     averageChange = (PnL[x]-PnL[y])
 
 # Appending values in List and incrementing loop for both x & y by 1
@@ -53,18 +51,15 @@ for month in range(totalMonth-1):
 
 # finding change of P & L and average for the entire period 
 avg_change_PnL = round(sum(changesPnL)/ (totalMonth-1),2)
-# print("Average Change : " , avg_change_PnL)
 
 # Solution 4: The greatest increase in profits (date and amount) over the entire period
 greatestIncrease = max(changesPnL)
 maxMonthindex = (monthCount[changesPnL.index(greatestIncrease)+1])
-# print ("Greatest increase in Profit : ", maxMonthindex, greatestIncrease)
 
    
 # Solution 5: The greatest decrease in profits (date and amount) over the entire period
 greatestDecrease = min(changesPnL) # min of Net total amt
 minMonthindex = (monthCount[changesPnL.index(greatestDecrease)+1]) # 
-# print ("Greatest Decrease in Profit : ", minMonthindex ,greatestDecrease)   
 
 # print the analysis to the terminal
 print("Financial Analysis")
@@ -87,62 +82,3 @@ with open('PybankAnalysis.txt', 'w') as file:
     print(f"Greatest increase in Profit : {maxMonthindex} (${greatestIncrease})", file=file)
     print(f"Greatest Decrease in Profit : {minMonthindex} (${greatestDecrease})", file=file)
     
-# creating finacial Analysis text report - Method 2
-import sys
-with open('output.txt', 'w') as file:
-    sys.stdout = file
-    print("Financial Analysis")
-    print("----------------------------" )    
-    print(f"Total Months :  {totalMonth}")
-    print(f"Total : ${totalPnL}")
-    print(f"Average Change : ${avg_change_PnL}")
-    print(f"Greatest increase in Profit : {maxMonthindex} (${greatestIncrease})")
-    print(f"Greatest Decrease in Profit : {minMonthindex} (${greatestDecrease})")
-    sys.stdout = sys.__stdout__  # Reset stdout to the default
-
-
-"""
-INSTRUCTION:-
- In this Challenge, you are tasked with creating a Python script to analyze the financial records of your company. You will be given a financial dataset called budget_data.csv. The dataset is composed of two columns: "Date" and "Profit/Losses".
- Your task is to create a Python script that analyzes the records to calculate each of the following values:
-
- '''
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire period
-# Your analysis should align with the following results:
-'''
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $22564198
-# Average Change: $-8311.11
-# Greatest Increase in Profits: Aug-16 ($1862002)
-# Greatest Decrease in Profits: Feb-14 ($-1825558)
-
-In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
